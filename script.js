2b6ae017160bf07a84299cd5f6f76ece2e6002d0
Deamon23 <s.d2003@yandex.ru>
2024-09-26 16:46:32 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Закрытие модального окна при клике вне его содержимого
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
+
